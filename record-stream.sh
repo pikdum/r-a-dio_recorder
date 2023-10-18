775669414a094b71713fd17a5318f58ec98f69af
pikdum <pikdum@kuudere.moe>
2023-10-17 22:36:40 -0500

    feat: log last played

--- record-stream.sh
+++ record-stream.sh
@@ -10,17 +10,29 @@ is_recording() {
     [[ -n "$pid" ]] && [[ -n $(ps -p $pid -o pid=) ]]
 }
 
+append_lp_log() {
+    local lp_file="$1"
+    curl -s https://r-a-d.io/api | jq -r '.main.lp[] | "\(.timestamp): \(.meta)"' >>"$lp_file"
+    sort -u -o "$lp_file" "$lp_file"
+}
+
 while true; do
     if is_streaming && ! is_recording; then
-        echo "Stream is active; recording started."
-        wget https://stream.r-a-d.io/main.mp3 -O "r-a-dio_$(date +"%Y-%m-%dT%H-%M-%S-%3N").mp3" >/dev/null 2>&1 &
+        recording="r-a-dio_$(date +"%Y-%m-%dT%H-%M-%S-%3N").mp3"
+        log="${recording}.log"
+        touch "$log"
+
+        echo "Recording started."
+        wget https://stream.r-a-d.io/main.mp3 -O "$recording" >/dev/null 2>&1 &
         pid=$!
+    elif is_streaming && is_recording; then
+        echo "Logging last played..."
+        append_lp_log "$log"
     elif ! is_streaming && is_recording; then
-        echo "Stream is not active; recording stopped."
+        echo "Recording stopped."
+        append_lp_log "$log"
         kill $pid
         pid=""
-    else
-        echo "Waiting until stream is active..."
-        sleep 30
     fi
+    sleep 30
 done
