d7ccfe5fddaa4f109f28a39ac1877940d9508a1d
pikdum <pikdum@kuudere.moe>
2025-01-06 20:34:56 -0600

    chore: change servers

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-scp record-stream.sh root@files.usagi.zone:/usr/local/bin/
-scp record-stream.service root@files.usagi.zone:/etc/systemd/system/
-ssh root@files.usagi.zone 'systemctl daemon-reload && systemctl restart record-stream'
+scp record-stream.sh root@yui.usagi.zone:/usr/local/bin/record-radio.sh
+scp record-stream.service root@yui.usagi.zone:/etc/systemd/system/record-radio.service
+ssh root@yui.usagi.zone 'systemctl daemon-reload && systemctl restart record-radio'
